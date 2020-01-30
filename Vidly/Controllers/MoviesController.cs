0809b93de9f7571052b72c0ad6228e4ff1e98a37
Victor Toderica <victor.toderica@axinf.onmicrosoft.com>
2020-01-30 20:29:30 +0200

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    //scaffolding = autogenerating some code
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        //view result is one of the action results that you work with most of the time 
+        //Action results:
+         //ViewResult          - returns a -           view
+         //PartialViewResult                           partial view                   
+         //ContentResult                               simple text
+         //RedirectResult                              redirect an user to an URL
+         //RedirectToRouteResult                       redirect an Action instead of URL
+         //JsonResult                                  serialized json object
+         //FileResult                                  file
+         //HttpNotFoundResult                          404 error
+         //EmptyResult                                 ~ void. the action doesn't need to return any value
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        //to make a parameter optional we should make it nullable
+        //reference types are nullable -> For example, you can assign any of the following three values to a bool? variable: true, false, or null.
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy= "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    //assuming that this page will be under  //movies/random we need to create a controller called movies and an action called random
+
+}
